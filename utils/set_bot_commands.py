c1179f72eeed29955dd2054a0fcce5a42f94a9e7
Woodey <nikita_woodey@mail.ru>
2024-10-02 13:12:18 +0300

    improve working with commands list

--- utils/set_bot_commands.py
+++ utils/set_bot_commands.py
@@ -1,10 +1,15 @@
 from aiogram.types import BotCommand, BotCommandScopeDefault
 
+cmds_list = [
+        ['start', 'Старт'],
+        ['cardio', 'Добавить кардио тренировку'],
+        ['climb', 'Добавить лазательную тренировку']
+]
+
 async def set_default_commands(bot): 
-    commands = [
-                BotCommand(command='start', description='Старт'),
-                BotCommand(command='help', description='Вывести справку')
-                # BotCommand(command='cardio', description='Добавить кардио тренировку')
-                ]
-    await bot.set_my_commands(commands, BotCommandScopeDefault())
+    bot_cmds = []
+    for i in range(0, len(cmds_list)):
+        bot_cmds.append(BotCommand(command=cmds_list[i][0], description=cmds_list[i][1]))
+
+    await bot.set_my_commands(bot_cmds, BotCommandScopeDefault())
 
